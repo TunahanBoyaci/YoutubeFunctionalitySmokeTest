77a88068a0ce3a19b058bdd12654dd5bd77de5a7
TunahanBoyaci <tunahanboyaci1@gmail.com>
2023-10-30 15:54:58 -0400

    YoutubeFunctionalitySmokeTest

--- src/test/java/FutureFiles/YoutubeFunctionality.feature
+++ src/test/java/FutureFiles/YoutubeFunctionality.feature
@@ -6,4 +6,3 @@ Feature: Youtube Search Functionality
     And Search for "Selenium" keyword
     And Click on Search Button
     Then Verify that at least "20" videos are displayed
-    Then Excel out the title of the last video
--- src/test/java/StepDefinitions/YoutubeFunctionality.java
+++ src/test/java/StepDefinitions/YoutubeFunctionality.java
@@ -59,33 +59,4 @@ public class YoutubeFunctionality {
 
         Assert.assertTrue(count > number);
     }
-
-    @Then("Excel out the title of the last video")
-    public void excelOutTheTitleOfTheLastVideo() {
-        XSSFWorkbook workbook = new XSSFWorkbook();
-        XSSFSheet sheet = workbook.createSheet("Sheet1");
-
-        for (int i = 0; i < dialogContent.listOfVideoElements.size(); i++) {
-            Row row = sheet.createRow(i);
-            Cell cell = row.createCell(0);
-            cell.setCellValue(dialogContent.listOfVideoElements.get(i).getAttribute("aria-label"));
-        }
-
-        String path = "excelFiles/excelFile.xlsx";
-
-        try {
-            File directory = new File("excelFiles");
-            if (!directory.exists()) {
-                directory.mkdirs(); // Create directories if they don't exist
-            }
-
-            FileOutputStream fileOutputStream = new FileOutputStream(new File(path));
-            workbook.write(fileOutputStream);
-            workbook.close();
-            fileOutputStream.close();
-            System.out.println("Excel file created successfully.");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
